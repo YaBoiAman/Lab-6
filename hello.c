b3ce1585d5dce6be6f9ddfceea8160ebb44e8222
Aman Chollangi <achollan@uoguelph.ca>
2025-10-30 20:05:05 -0400

    Added Hello World program for Lab 6

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-void main (void){
-    
+int main(void) {
+    printf("Hello, world!\n");
+    printf("This program is part of ENGG1410 Lab 6!\n");
+    printf("\n V2");
     return 0;
 }
